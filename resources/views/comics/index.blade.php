47fe636a19b0d0acd5858fd2cd00ea22cda8fad3
LuisJann <giannoneluigi10@gmail.com>
2022-12-21 17:34:36 +0100

    add header & table

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('title', 'tutti i comics')
+@section('content')
+    <div class="container">
+        <h2>Tutti i comics</h2>
+
+    </div>
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Prezzo</th>
+                    <th scope="col">Data di rilascio</th>
+                    <th scope="col">Tipo</th>
+                    <th scope="col">Azione</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($comics as $comic)
+                    <tr>
+                        <th scope="row">{{ $comic->title }}</th>
+                        <td>{{ $comic->price }}</td>
+                        <td>{{ $comic->sale_date }}</td>
+                        <td>{{ $comic->type }}</td>
+                        <td><button class="btn btn-primary "><a class="nav-link text-white" href="">Scopri di più</a>
+                            </button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="container bg-light mb-4">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav d-flex w-100">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        <a class="nav-link" href="{{ route('comics.index') }}">Comics</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</header>
